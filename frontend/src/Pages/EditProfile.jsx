d6e8a85c29a524a424cf98acfefe02963a022e9b
agronomthe6th <victor020100@gmail.com>
2023-01-06 20:50:07 +0300

    EditProfile

--- frontend/src/Pages/EditProfile.jsx
+++ frontend/src/Pages/EditProfile.jsx
@@ -1,185 +1,88 @@
 import React from 'react';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-import {Container, Row, Col, Card, Button, Form,Navbar ,Nav, NavDropdown,Dropdown,DropdownButton} from 'react-bootstrap'
-import Note from '../Components/Note'
-import { confirmAlert } from 'react-confirm-alert'; // Import
-import { UpdateAuthStatus } from '../store/authentication/auth';
-import Cookie from 'js-cookie';
-import { useSelector, useDispatch } from 'react-redux';
-import { Link, useNavigate, useParams } from 'react-router-dom';
-import PlaceholderLoading from 'react-placeholder-loading'
-import { fetchUserBytoken, userSelector } from '../store/UserSlice';
-import { FetchVideosByUser, videoSelector } from '../store/VideoSlice';
-import VideoWatchItemCard from '../Components/video/VideoWatchItemCard';
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { Row, Col, Card} from 'react-bootstrap'
 
 function EditProfile ({}) {
-    const dispatch = useDispatch();
-    const { username, uid, avatar, authenticated, userInfo } = useSelector(userSelector);
-    const [notes, setNotes] = useState([]);
     const navigate = useNavigate();
-    const [notesWithoutFilter, setNotesWithoutFilter] = useState('');
-    const uservideos = useSelector(state => state.video);
-    const {videos, isFetching, isError, isSuccess} = uservideos;
+    const [imgState, setImgState] = useState({avatar:null, preview:null})
+    const [desc, setDesc] = useState("")
+    const [msg, setMsg] = useState("")
+
+    const imageChange = (event) => {
+        setImgState({avatar:event.target.files[0], preview:URL.createObjectURL(event.target.files[0])})
+      }
+    
+    const userInfo = useSelector((state) => state.auth.user.userInfo);
+    const is_authenticated = useSelector((state) => state.auth.user.is_authenticated);
+    const [profile, setProfile] = useState([]);
 
     useEffect(() => {
-     dispatch(fetchUserBytoken);
-     dispatch(FetchVideosByUser({userId:uid}));
-    }, [])
-        
-    const HandleChange = (e) => {
-      // console.log(e);
-      axios.get(
-        `http://127.0.0.1:8000/api/?user=${uid}&ordering=${e}`
-      )
-      .then((response) => {
-        setNotes(response.data.results) 
-      })
-      .catch((error) => {
-        console.log(error);
-      })
-    }
+        // console.log(channel_jsx_info)
+        var id = JSON.parse(userInfo)['id'];
+        axios.get(
+            `http://127.0.0.1:8000/api/profiledetail/${id}`
+        )
+            .then((response) => {
+                // console.log(response.data);
+                setProfile(response.data) 
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }, [userInfo, is_authenticated])
 
-    function EditProfile(){
-      console.log(videos);
-      // navigate('/edit-profile');
-    }
-
-    function HandleLogout(){
-      confirmAlert({
-        title: 'Confirm to submit',
-        message: 'Are you sure to do this.',
-        buttons: [
-          {
-            label: 'Yes',
-            onClick: () => {
-              Cookie.remove('access_key');
-              Cookie.remove('userInfo');
-              dispatch(UpdateAuthStatus({ Access: false, userInfo: {}}));
-              navigate('/');
-            }
+    const handleFormSubmit = (e) => {
+        e.preventDefault()
+        let id = JSON.parse(userInfo)['id'];
+        let form_data = new FormData();
+        form_data.append("avatar", imgState.avatar, imgState.avatar.name);
+        form_data.append("about", desc);
+        // console.log(form_data);
+        let url = `http://127.0.0.1:8000/api/updateprofile/${id}`;
+        axios
+        .put(url, form_data, {
+          headers: {
+            "content-type": "multipart/form-data",
           },
-          {
-            label: 'No',
-            onClick: () => alert('Click No')
-          }
-        ]
-      });
-    }
+        })
+        .then((res) => {
+            navigate('/my-profile');
+            // console.log(res);
+        })
+        .catch((err) => {
+          setMsg("Error");
+          setTimeout(()=>{setMsg("")},4000);
+          // console.log(err)
+        });
+      }
 
-    function sortResult(e){
-      e.preventDefault();
-      var titleFilter = e.target.value;
-      var filteredData=[...notesWithoutFilter].filter(
-          function(el){
-              return el.title.toString().toLowerCase().includes(
-                  titleFilter.toString().trim().toLowerCase()
-              )
-          }
-      );
-      setNotes(filteredData)
-  }
-
-  // if (loading) {
-  //   return (
-  //     <Row className="d-flex justify-content-center m-4">
-  //         <Col className='profile-col col-sm-10 col-md-8 col-lg-6'>
-  //           <Card className="profile-card" filter-color="blue">
-  //             <PlaceholderLoading shape="rect" width={'100%'} height={'100%'}/>
-  //           </Card>
-  //       </Col>
-  //     </Row>
-  //     )
-  //   }
     return (
-      <>
-        { isFetching ? (
-          <Row className="d-flex justify-content-center m-4">
-            <Col className="col-sm-10 col-md-8 col-lg-6">
-                <Card className="p-3">
-                  <PlaceholderLoading shape="rect" width={'100%'} height={'100%'}/>
-                </Card>
-            </Col>
-          </Row>
-        ) : isError ? (
-          <h1>Error</h1>
-        ) : (
-          videos && (
-          <Container >
-          <Row className="d-flex justify-content-center">
-            <Col className='profile-col col-sm-10 col-md-8 col-lg-6'>
-              <Card className="profile-card border border-white">
-              <div className="col d-flex justify-content-end" >
-                <DropdownButton className="text-right" id="dropdown-item-button " style={{position: 'absolute'}} title=''>
-                  <Dropdown.Item as="button"  onClick={HandleLogout}>Logout</Dropdown.Item>
-                  <Dropdown.Item as="button"  onClick={EditProfile}>Edit profile</Dropdown.Item>
-                </DropdownButton>
+        <Row className="d-flex justify-content-center">
+          <Col className="col-sm-10 col-md-8 col-lg-6">
+            <Card className="p-3">
+            <h3 style={{backgroundColor:'red',color:'white',textAlign:'center'}}>{msg}</h3>
+              <form onSubmit={(e)=>handleFormSubmit(e)}>
+                <div className="form-group p-1 ">
+                <img src={profile.avatar} alt="" width="150" height="160"/>
                 </div>
-                  <div className="page-header-image" src={avatar}></div>
-                  <Card.Body className="profile-header" >
-                  <Card.Img  src={avatar} alt="Card image" 
-                    style={{width: '123px',
-                      height: '123px',
-                      overflow: 'hidden',
-                      margin: '0 auto'}}/>
-                    <h3 className="title">{username}</h3>
-                    <Row className="stats-row">
-                        <Col className='stats'>
-                          <h2>1</h2>
-                          <p>some</p>
-                        </Col>
-                        <Col className='stats'>
-                          <h2>2</h2>
-                          <p>random</p>
-                        </Col>
-                        <Col className='stats'>
-                          <h2>3</h2>
-                          <p>stats</p>
-                        </Col>
-                    </Row>
-                  </Card.Body>
-              </Card>
-            </Col>
-          </Row>
-
-          <Navbar expand="lg" className='m-3'>
-            <Container fluid>
-                <Form className="d-flex">
-              <div className="input-group mb-3">
-                  <span className="input-group-text">title</span>
-                  <input type="text" className="form-control"
-                  onChange={sortResult}/>
-              </div>
-
-                </Form>
-                <Nav
-                  className="my-2 px-3 my-lg-0"
-                  style={{ maxHeight: '100px' }}
-                  navbarScroll
-                >
-                  <select  className="dropdown-me" onChange={(e) => HandleChange(e.target.value)}> 
-                    <option value="">-</option>
-                    <option value="id">id</option>
-                    <option value="title">title</option>
-                    <option value="description">description</option>
-                  </select>
-                </Nav>
-            </Container>
-          </Navbar>
-          {isSuccess &&
-            <Row className="d-flex justify-content-center"> 
-              {videos.results.map(item => (
-                <Col className="d-flex justify-content-center mb-3" key={item.id} sm={8} md={6} lg={4} xl ={3}>
-                  <VideoWatchItemCard  key={item.id} video={item} />
-                </Col>
-              ))}
-            </Row>
-          }
-        </Container>
-      ))}
-      </>
-    )
-
-}
+                <div className="form-group p-1 ">
+                <label>Select avatar:</label>
+                <input type="file" accept="avatar/*" onChange={imageChange} />
+                </div>
+                <div className="form-group p-1 ">
+                  <label>Description:</label>
+                  <input type="text" className="form-control" placeholder={profile.about} onChange={(e)=>setDesc(e.target.value)} />
+                </div>
+                <br/>
+                <button type="submit" className="btn btn-primary ">Submit</button>
+              </form>
+            </Card>
+          </Col>
+        </Row>
+      )
+    }
 
 export default EditProfile;
